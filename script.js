7d90cc076c8bd15471b62de185921c38fca0fbb5
Grey Stag <fritz.then.fenske@gmail.com>
2023-02-01 15:16:33 -0500

    making mathamatical functions operational

--- script.js
+++ script.js
@@ -1,85 +1,131 @@
 const buttons = document.getElementsByClassName('btn');
 
-
+let a = ""
+let b = ""
+let c = ""
 for (const btn of buttons){btn.addEventListener('click', e => {
     let text = e.target.id;
+    
+    if(text == 0 || text == 1 || text == 2 || text == 3 || text == 4 || text == 5 || text == 6 || text == 7 || text == 8 || text == 9 || text == ".") {
+        a += text
+        console.log(a)
+    }
 
     if (text == "ac"){
         console.log("All Clear")
     }
     else if (text == "%"){
-        console.log("modulo")
+        modulo(a)
     }
     else if (text == "/"){
-        console.log("divide")
+        divide(a)
     }
     else if (text == "x"){
-        console.log("times")
+        multiply(a)
     }
     else if (text == "-"){
-        console.log("subtract")
+        subtract(a)
     }
     else if (text == "+"){
-        console.log("add")
+        addition(a)
     }
+
+
     else if (text == "="){
         console.log("equals")
     }
     else if (text == "."){
         console.log("period")
     }
-    else if (text == "9"){
-        console.log("nine")
-    }
-    else if (text == "8"){
-        console.log("eight")
-    }
-    else if (text == "7"){
-        console.log("seven")
-    }
-    else if (text == "6"){
-        console.log("six")
-    }
-    else if (text == "5"){
-        console.log("five")
-    }
-    else if (text == "4"){
-        console.log("four")
-    }
-    else if (text == "3"){
-        console.log("three")
-    }
-    else if (text == "2"){
-        console.log("two")
-    }
-    else if (text == "1"){
-        console.log("one")
-    }
-    else if (text == "0"){
-        console.log("zero")
-    }
-    console.log(text);
-    
-    
 });
 }
 
-const addition = function(a, b){
 
-}
-
-const subtract = function(a, b){
-
-}
-
-const multiply = function(a, b){
-
-}
-
-const divide = function(a, b){
-
-}
-
-const modulo = function(a, b){
+const addition = function(a){
+    let addNum = []
+    for (const btn of buttons){btn.addEventListener('click', e => {
+        let text = e.target.id;
     
+        if(text == 0 || text == 1 || text == 2 || text == 3 || text == 4 || text == 5 || text == 6 || text == 7 || text == 8 || text == 9 || text == ".") {
+        b += text
+        console.log(b)}
+        
+        if (text == "="){
+            let addNum = (+a + +b)
+            console.log(addNum)
+        }
+    })}
+    return  addNum
+}
+
+const subtract = function(a){
+    let subNum =[]
+    for (const btn of buttons){btn.addEventListener('click', e => {
+        let text = e.target.id;
+    
+        if(text == 0 || text == 1 || text == 2 || text == 3 || text == 4 || text == 5 || text == 6 || text == 7 || text == 8 || text == 9 || text == ".") {
+        b += text
+        console.log(b)}
+        
+
+        if (text == "="){
+            subNum = (+a - +b)
+            console.log(subNum)
+        }
+    })}
+    return subNum
+}
+
+const multiply = function(a){
+    let multiNum =[]
+    for (const btn of buttons){btn.addEventListener('click', e => {
+        let text = e.target.id;
+    
+        if(text == 0 || text == 1 || text == 2 || text == 3 || text == 4 || text == 5 || text == 6 || text == 7 || text == 8 || text == 9 || text == ".") {
+        b += text
+        console.log(b)}
+        
+
+        if (text == "="){
+            multiNum = (+a * +b)
+            console.log(multiNum)
+        }
+    })}
+    return multiNum
+}
+
+const divide = function(a){
+    let divideNum =[]
+    for (const btn of buttons){btn.addEventListener('click', e => {
+        let text = e.target.id;
+    
+        if(text == 0 || text == 1 || text == 2 || text == 3 || text == 4 || text == 5 || text == 6 || text == 7 || text == 8 || text == 9 || text == ".") {
+        b += text
+        console.log(b)}
+        
+
+        if (text == "="){
+            divideNum = (+a / +b)
+            console.log(divideNum)
+        }
+    })}
+    return divideNum
+}
+
+const modulo = function(a){
+    let moduNum =[]
+    for (const btn of buttons){btn.addEventListener('click', e => {
+        let text = e.target.id;
+    
+        if(text == 0 || text == 1 || text == 2 || text == 3 || text == 4 || text == 5 || text == 6 || text == 7 || text == 8 || text == 9 || text == ".") {
+        b += text
+        console.log(b)}
+        
+
+        if (text == "="){
+            moduNum = (+a - +b)
+            console.log(moduNum)
+        }
+    })}
+    return moduNum
 }
